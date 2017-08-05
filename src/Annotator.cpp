d182987d4f6cfe0db4dde0152ba2a6e13d61ef05
starcolon <tao@starcolon.com>
2017-08-06 01:57:22 +0700

    annotator : image reading iter

--- src/Annotator.cpp
+++ src/Annotator.cpp
@@ -9,6 +9,21 @@
 
 const string ARGUMENT_KEYS = 
   "{@path           |.       | path to image directories  }";
+const string SUPPORTED_EXTS[] = {
+  ".png", ".PNG", ".jpg", ".JPG",
+  ".jpeg", ".JPEG"
+};
+
+
+static bool supportedFileFormat(const string& path)
+{
+  for (auto ext : SUPPORTED_EXTS)
+  {
+    if (path.find(ext) != string::npos)
+      return true;
+  }
+  return false;
+}
 
 int main(int argc, char** argv)
 {
@@ -16,14 +31,20 @@ int main(int argc, char** argv)
   parser.about("Image Annotator Utility");
 
   string path = parser.get<string>(0);
-
-  cout << "Reading images from " << path << endl;
+  cout << GREEN << "Reading images from "
+       << YELLOW << path << RESET << endl;
   vector<String> files;
   glob(path, files);
 
   for (auto file : files)
   {
-    auto filename = file.operator string();
+    auto filepath = file.operator string();
+    auto filename = filepath.substr(filepath.find_last_of("/") + 1);
+    if (!supportedFileFormat(filename))
+      continue;
+    cout << CYAN << "Loading image : " << RESET << filename << endl;
+
+    // TAOTODO:
   }
 
 }
