5cd868fe56f119248ffdce2005fe97c61614e39e
starcolon <tao@starcolon.com>
2017-10-02 08:36:39 +0700

    added initial ParamShape implementation

--- headers/ParamShape.h
+++ headers/ParamShape.h
@@ -4,18 +4,23 @@
 #include "master.h"
 #include "Shape.h"
 
+class ParameterisedShape;
+
 class ShapeEncoder
 {
 protected:
   Mat mean;
   Mat eigen;
+  Mat eigen_1; // Invert of eigen
 public:
-  ShapeEncoder(mean, eigen) : mean(mean), eigen(eigen);
+  ShapeEncoder(Mat mean, Mat eigen) : mean(mean), eigen(eigen), eigen_1(eigen.inv()) {};
   virtual ~ShapeEncoder(){};
+  Shape decode(const ParameterisedShape &s) const;
+  Mat encode(const Shape& s) const;
 };
 
 /**
- * Parameterised Shape, encoded by PCA
+ * Parameterised Shape, encoded by PCA parameters
  */
 class ParameterisedShape
 {
@@ -23,8 +28,10 @@ private:
 protected:
   Mat params;
 public:
-  ParameterisedShape(const Shape& shape);
+  ParameterisedShape(const Shape& shape, const ShapeEncoder& enc);
   inline virtual ~ParameterisedShape(){};
+
+  Mat getParams() const { return this->params; };
 
   // ---------- Conversion -----------
   Shape toShape(const ShapeEncoder& enc) const;
---
+++ src/lib/ParamShape.cpp
@@ -0,0 +1,21 @@
+#include "ParamShape.h"
+
+Shape ShapeEncoder::decode(const ParameterisedShape &s) const
+{
+  return Shape(this->mean + this->eigen * s.getParams());
+}
+
+Mat ShapeEncoder::encode(const Shape& s) const
+{
+  return this->eigen_1 * (s.mat - this->mean);
+}
+
+ParameterisedShape::ParameterisedShape(const Shape& shape, const ShapeEncoder& enc)
+{
+  this->params = enc.encode(shape);
+}
+
+Shape ParameterisedShape::toShape(const ShapeEncoder& enc) const
+{
+  // TAOTODO:
+}
