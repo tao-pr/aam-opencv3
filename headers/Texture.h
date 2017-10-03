6de7386ae01a4dfe8aff74420855852a85bf7102
starcolon <tao@starcolon.com>
2017-10-04 00:14:12 +0700

    implemented some fundamental functions for Triangle and Texture

--- headers/Texture.h
+++ headers/Texture.h
@@ -8,20 +8,39 @@
 #include "master.h"
 #include "IO.h"
 #include "aux.h"
-#include "MeshShape.h"
 
+class Triangle
+{
+public:
+  Mat vertices; // 3 x 2
+  inline Triangle(const Mat& v) : vertices(v) {};
+  inline ~Triangle(){};
+
+  inline Point2d get(int i) const { return Point2d(vertices.at<double>(i,0), vertices.at<double>(i,1)); }
+  void boundary(double& minX, double& minY, double& maxX, double& maxY) const;
+  double minX() const;
+  double minY() const;
+  double maxX() const;
+  double maxY() const;
+
+  Triangle operator >>(const Point2d &displacement) const;
+  Triangle operator <<(const Point2d &displacement) const;
+};
+
+/**
+ * Texture coupled with a triangular face
+ */
 class Texture
 {
 private:
 protected:
 public:
   Mat img;
-  MeshShape shape;
+  Triangle bound;
 
-  inline Texture(){};
-  inline Texture(const MeshShape& shape) : shape(shape) {};
-  inline Texture(const MeshShape& shape, const Mat& a) : shape(shape) { a.copyTo(this->img); };
-  inline Texture(const Texture& original){ original.img.copyTo(this->img); };
+  inline Texture(const Triangle& bound) : bound(bound) {};
+  inline Texture(const Triangle& bound, const Mat& a) : bound(bound) { a.copyTo(this->img); };
+  inline Texture(const Texture& original) : bound(original.bound) { original.img.copyTo(this->img); };
   virtual inline ~Texture(){};
 
   // --------- General properties -------
@@ -32,8 +51,9 @@ public:
   virtual void load(const string path);
   virtual Mat render(IO::GenericIO* io, Mat background, double scaleFactor=1.0, Point2d recentre=Point2d(0,0)) const;
 
-  //------ Operators -------
+  //------ Operators / Transformations -------
   Texture operator*(double scale) const;
+  Texture realignTo(const Triangle &newBound) const;
 };
 
 #endif
--- src/lib/Texture.cpp
+++ src/lib/Texture.cpp
@@ -1,5 +1,72 @@
 #include "Texture.h"
 
+Triangle Triangle::operator >>(const Point2d &displacement) const
+{
+  Mat dist = Mat(3, 2, CV_64FC1);
+  for (int i=0; i<3; i++)
+  {
+    dist.at<double>(i,0) = displacement.x;
+    dist.at<double>(i,1) = displacement.y;
+  }
+  return Triangle(this->vertices + dist);
+}
+
+Triangle Triangle::operator <<(const Point2d &displacement) const
+{
+  Mat dist = Mat(3, 2, CV_64FC1);
+  for (int i=0; i<3; i++)
+  {
+    dist.at<double>(i,0) = displacement.x;
+    dist.at<double>(i,1) = displacement.y;
+  }
+  return Triangle(this->vertices - dist);
+}
+
+void Triangle::boundary(double& minX, double& minY, double& maxX, double& maxY) const
+{
+  minX = numeric_limits<double>::max();
+  minY = numeric_limits<double>::max();
+  maxY = -numeric_limits<double>::max();
+  maxX = -numeric_limits<double>::max();
+  for (int i=0; i<3; i++)
+  {
+    double x = this->vertices.at<double>(i,0);
+    double y = this->vertices.at<double>(i,1);
+    minX = x < minX ? x : minX;
+    minY = y < minY ? y : minY;
+    maxX = x > maxX ? x : maxX;
+    maxY = y > maxY ? y : maxY;
+  }
+}
+
+double Triangle::minX() const
+{
+  double a,b,c,d;
+  boundary(a,b,c,d);
+  return a;
+}
+
+double Triangle::minY() const
+{
+  double a,b,c,d;
+  boundary(a,b,c,d);
+  return b;
+}
+
+double Triangle::maxX() const
+{
+  double a,b,c,d;
+  boundary(a,b,c,d);
+  return c;
+}
+
+double Triangle::maxY() const
+{
+  double a,b,c,d;
+  boundary(a,b,c,d);
+  return d;
+}
+
 void Texture::save(const string path) const
 {
   // TAOTODO:
@@ -17,5 +84,16 @@ Mat Texture::render(IO::GenericIO* io, Mat background, double scaleFactor, Point
 
 Texture Texture::operator*(double scale) const
 {
-  return Texture(this->shape, this->img * scale);
+  return Texture(this->bound, this->img * scale);
+}
+
+Texture Texture::realignTo(const Triangle &newBound) const
+{
+  double minX, minY, maxX, maxY;
+  newBound.boundary(minX, minY, maxX, maxY);
+  Mat newImg = Mat(Size(maxY-minY, maxX-minX), CV_64FC1);
+
+  // TAOTODO:
+
+  return Texture(newBound, newImg);
 }
