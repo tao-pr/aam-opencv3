8019363c7a596b80e6360d03ff89e986dfca897b
starcolon <tao@starcolon.com>
2017-10-12 00:16:55 +0700

    added initial empty AAM2D implementations

--- src/lib/AAM2D.cpp
+++ src/lib/AAM2D.cpp
@@ -1,6 +1,32 @@
 #include "AAM2D.h"
 
-GenericAAM Trainset2D::train(bool verbose) const
+void AAM2D::fit(Mat im)
 {
 
+}
+
+void AAM2D::saveToFile(const string& modelFileName) const
+{
+
+}
+
+void AAM2D::loadFromFile(const string& modelFileName)
+{
+
+}
+
+void AAM2D::trainFromFileList(const vector<string>& filelist)
+{
+  
+}
+
+GenericAAM Trainset2D::train(bool verbose) const
+{
+  // Create Shape model
+
+  // Create Texture model
+
+  // Consolidate Shape and Texture models
+
+  // Return a downcasted [[AAM2D]] model
 }
