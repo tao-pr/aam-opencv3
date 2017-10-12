ebed2ab4c918a0812d46e288219e52423ebedf4e
starcolon <tao@starcolon.com>
2017-10-13 03:06:28 +0700

    draft of AppearanceCollection

--- headers/AppearanceCollection.h
+++ headers/AppearanceCollection.h
@@ -17,6 +17,21 @@ public:
   inline AppearanceCollection(){};
   inline AppearanceCollection(const vector<Texture>& apps) : items(apps) {};
   inline AppearanceCollection(const AppearanceCollection& original) : items(original.items) {};
+
+  inline void add(const Texture &t){ this->items.push_back(t); };
+
+  // ---------- Analysis of textures ------------
+  
+
+  // ---------- Transformations ---------------
+  AppearanceCollection clone(bool verbose=false) const;
+  AppearanceCollection normaliseScalingTranslation() const;
+  AppearanceCollection normaliseRotation() const;
+  AppearanceCollection translateBy(const Point2d &p) const;
+
+  // ---------- I/O ------------------
+  vector<Texture> getItems() const;
+  Mat toMat() const;
 };
 
 #endif
