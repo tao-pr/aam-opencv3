94ca62a15f8771acb394ac8ae425048545cc1776
starcolon <tao@starcolon.com>
2017-10-15 23:54:29 +0700

    added initial AppearanceModel cpp

--- headers/AppearanceModel.h
+++ headers/AppearanceModel.h
@@ -14,10 +14,17 @@ class Appearance
 {
 private:
 protected:
+  Mat* imgRef; // Reference to an appearance image (external variable)
   MeshShape mesh;
+  vector<Texture> textureList;
+
+  void reinitiateTextures();
 public:
-  Appearance(){};
+  Appearance(const MeshShape& shape, Mat* img) : imgRef(img), mesh(shape) { reinitiateTextures(); };
   virtual inline ~Appearance(){};
+
+  //------ I/O --------------
+  Mat render(IO::GenericIO* io, Mat background, double scaleFactor=1.0, Point2d recentre=Point2d(0,0)) const;
 };
 
 #endif
---
+++ src/lib/AppearanceModel.cpp
@@ -0,0 +1,11 @@
+#include "AppearanceModel.h"
+
+void Appearance::reinitiateTextures()
+{
+
+}
+
+Mat Appearance::render(IO::GenericIO* io, Mat background, double scaleFactor, Point2d recentre) const
+{
+
+}
