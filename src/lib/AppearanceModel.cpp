8adc26acc8be45b3ffa14f57659fe26bc5677db2
starcolon <tao@starcolon.com>
2017-10-20 23:59:25 +0700

    initialise textures

--- src/lib/AppearanceModel.cpp
+++ src/lib/AppearanceModel.cpp
@@ -2,7 +2,12 @@
 
 void Appearance::reinitiateTextures()
 {
-
+  this->textureList.clear();
+  auto triangles = this->mesh.getTriangles();
+  for (auto triangle : triangles)
+  {
+    this->textureList.push_back(Texture(triangle, this->imgRef));
+  }
 }
 
 Mat Appearance::render(IO::GenericIO* io, Mat background, double scaleFactor, Point2d recentre) const
