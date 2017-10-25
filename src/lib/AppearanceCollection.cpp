65b88be57b4daf01b933d73443980169c13b33b1
starcolon <tao@starcolon.com>
2017-10-25 21:28:28 +0800

    (wip) draft AppearanceCollection toMat

--- src/lib/AppearanceCollection.cpp
+++ src/lib/AppearanceCollection.cpp
@@ -4,13 +4,18 @@ Mat AppearanceCollection::toMat() const
 {
   // Each element has to have equal boundary
   //
+  Mat frontMat = this->items[0].toRowVector();
+  int N = this->items.size();
+  int M = frontMat.cols;
+  auto type = frontMat.type();
+  Mat m = Mat(N, M, type);
+  int j = 0;
   for (auto item : this->items)
   {
-    
+    item.toRowVector().row(0).copyTo(m.row(j));
+    ++j;
   }
-
-
-
+  return m;
 }
 
 // TAOTODO:
