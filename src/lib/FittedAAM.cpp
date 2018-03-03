e976694897cbd94783578fe877fc8a7d1cee0ef1
starcolon <tao@starcolon.com>
2018-03-04 02:31:48 +0700

    (wip) added initial FittedAAM cpp

---
+++ src/lib/FittedAAM.cpp
@@ -0,0 +1,26 @@
+#include "FittedAAM.h"
+
+FittedAAM::FittedAAM(const FittedAAM& another)
+{
+
+}
+
+void FittedAAM::setCentre(const Point2d& p)
+{
+
+}
+
+void FittedAAM::setScale(const double& s)
+{
+
+}
+
+void FittedAAM::setShapeParam(const Mat& param)
+{
+
+}
+
+void FittedAAM::setAppearanceParam(const Mat& param)
+{
+  
+}
