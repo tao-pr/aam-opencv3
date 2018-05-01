c2c1c24241a04123a412ea81330203bb1b4f4d44
starcolon <tao@starcolon.com>
2018-05-01 23:00:52 +0700

    initial tree search

---
+++ headers/TreeSearch.h
@@ -0,0 +1,22 @@
+#ifndef TREE_SEARCH
+#define TREE_SEARCH
+
+#include "master.h"
+#include "BaseModel.h"
+#include "Shape.h"
+#include "MeshShape.h"
+#include "Appearance.h"
+#include "ModelPCA.h"
+#include "BaseFittedModel.h"
+#include "ModelFitter.h"
+
+class TreeSearch 
+{
+private:
+protected:
+public:
+  TreeSearch();
+  virtual ~TreeSearch(){};
+};
+
+#endif
